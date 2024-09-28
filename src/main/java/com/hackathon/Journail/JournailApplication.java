b22da9c76bc77743568f236601c1d16b9cecf477
Drew Huniak <drewnhuniak@gmail.com>
2024-09-28 11:17:01 -0500

    Adding backend part 2

---
+++ src/main/java/com/hackathon/Journail/JournailApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.Journail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournailApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hackathon/Journail/JournailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.Journail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
