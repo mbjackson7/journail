a3bd661248249621982c9458f0cdb03e7ffa51a5
Scott Delozier <sdelozi@siue.edu>
2024-09-28 23:14:35 -0500

    UNUSED IMPORTS UNUSED IMOPRTS IUNSRU DOFISJF STEIDSOF HSOIFD SDFJSDIOFJOSIJOFIJDSOJFSIJDF DSFJOSDJFLSJ

--- server/src/test/java/com/hackathon/Journail/BO/PromptBOTests.java
+++ server/src/test/java/com/hackathon/Journail/BO/PromptBOTests.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
