1e91b146546908004bc72bfaacb6a5e18d65a694
Scott Delozier <sdelozi@siue.edu>
2024-09-28 21:30:34 -0500

    closer questions

--- server/src/main/java/com/hackathon/Journail/BO/PromptBO.java
+++ server/src/main/java/com/hackathon/Journail/BO/PromptBO.java
@@ -14,18 +14,33 @@ import java.util.List;
 public class PromptBO {
 
     public String getStarterQuestion(JournalEntry journalEntry) {
-        List<String> defaultOpeners = new ArrayList<>();
-        String sampleOpeners = "sampleopeners.txt";
+        List<String> defaultOpeners = getSampleQuestions("sampleopeners.txt");
+
+        //prompt bedrock for opener based on context and random default opener here
+        String starterQuestion = "HOW ARE YOUR BALLS TODAY?!?";
+
+        return starterQuestion;
+    }
+
+    public String getCloserQuestion(JournalEntry journalEntry) {
+        List<String> defaultClosers = getSampleQuestions("sampleclosingquestions.txt");
+
+        //prompt bedrock for closer based on context and random default closer here
+        String closerQuestion = "WILL YOU PLEASE BRING ME GALAXY GAS NEXT TIME?!?!";
+
+        return closerQuestion;
+    }
+
+    private List<String> getSampleQuestions(String sampleQuestionFile) {
+        List<String> defaultQuestions = new ArrayList<>();
+
         try {
-            defaultOpeners = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource(sampleOpeners).toURI()));
+            defaultQuestions = Files.readAllLines(Paths.get(getClass().getClassLoader().getResource(sampleQuestionFile).toURI()));
         } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
 
-        //prompt bedrock for opener based on context
-        String starterQuestion = "HOW ARE YOUR BALLS TODAY?!?";
-
-        return starterQuestion;
+        return defaultQuestions;
     }
 
 }
--- server/src/test/java/com/hackathon/Journail/BO/PromptBOTests.java
+++ server/src/test/java/com/hackathon/Journail/BO/PromptBOTests.java
@@ -1,14 +1,27 @@
 package com.hackathon.Journail.BO;
 
+import com.hackathon.Journail.Model.JournalEntry;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 public class PromptBOTests {
+
+    @MockBean
+    private JournalEntry journalEntry;
+
     @Test
     void getStarterQuestionTest_notNullResponse() {
         PromptBO promptBO = new PromptBO();
-        String testStarterQuestion = promptBO.getStarterQuestion();
+        String testStarterQuestion = promptBO.getStarterQuestion(journalEntry);
+        assertThat(testStarterQuestion).isNotNull();
+    }
+
+    @Test
+    void getCloserQuestionTest_notNullResponse() {
+        PromptBO promptBO = new PromptBO();
+        String testStarterQuestion = promptBO.getCloserQuestion(journalEntry);
         assertThat(testStarterQuestion).isNotNull();
     }
 }
