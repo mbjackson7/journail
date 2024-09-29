c25fa6b63249a5096c837eca16959b7e1199d765
CodyScha <codyschaefer22@gmail.com>
2024-09-28 22:31:14 -0500

    pinecone save and query on user id

--- server/src/main/java/com/hackathon/Journail/BO/PineconeBoImpl.java
+++ server/src/main/java/com/hackathon/Journail/BO/PineconeBoImpl.java
@@ -1,14 +1,18 @@
 package com.hackathon.Journail.BO;
 
 import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
 public class PineconeBoImpl implements PineconeBo {
+    private final String USER_ID = "userId";
     private final VectorStore pineconeStore;
 
     public PineconeBoImpl(VectorStore pineconeStore) {
@@ -17,14 +21,18 @@ public class PineconeBoImpl implements PineconeBo {
 
 
     @Override
-    public List<String> get(String query) {
-        List<Document> results = pineconeStore.similaritySearch(query);
+    public List<String> get(String query, String userId) {
+        List<Document> results = pineconeStore.similaritySearch(
+                SearchRequest.defaults()
+                        .withQuery(query)
+                        .withFilterExpression(String.format("%s == '%s'", USER_ID, userId)));
 
         return results.stream().map(Document::getContent).collect(Collectors.toList());
     }
 
     @Override
-    public void save(String textToEmbed) {
-        pineconeStore.add(List.of(new Document(textToEmbed)));
+    public void save(String textToEmbed, String userId) {
+        Document toStore = new Document(textToEmbed, Map.of(USER_ID, userId));
+        pineconeStore.add(List.of(toStore));
     }
 }
